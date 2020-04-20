25cae0d415fbadcddeb4eadea21bae6b421db1d7
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-04-21 00:15:57 +0700

    Membuat AutoComplete AJAX (not finished yet)

---
+++ starbhak-ajax/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar AutoComplete Ajax di SMK Taruna Bhakti</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>PHP AJAX SMK Taruna Bhakti</h1>
+        <h2 align="center" class="mb-4 mt-4">Autocomplete Dengan Gambar dan Ajax</h2>
+        <div align="center">
+            <input type="text" name="" id="nama_siswa" placeholder="Cari Nama Siswa..." class="form-control">
+        </div>
+        <ul class="list-group" id="resultlist"></ul>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="script.js"></script>
+
+    
+
+</body>
+</html>
